1062f93ca03633acd789cb0e345f33f83d56e00f
RejaniVarghese <rejanimv@yahoo.co.in>
2024-06-05 05:27:02 +0530

    modified server url

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import axios from 'axios'
 import { UserContextProvider } from './contexts/userContext'
 import TaskPage from './pages/TaskPage'
 
-axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+axios.defaults.baseURL = "https://tasksps-api.vercel.app";
 axios.defaults.withCredentials = true
 
 function App() {
