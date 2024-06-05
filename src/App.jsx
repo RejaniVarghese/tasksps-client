903f4b3eed373b383cd379272801f8b857c5bc6b
RejaniVarghese <rejanimv@yahoo.co.in>
2024-06-05 07:27:34 +0530

    added access control

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,10 @@ import TaskPage from './pages/TaskPage'
 
 axios.defaults.baseURL = "https://tasksps-api.vercel.app";
 axios.defaults.withCredentials = true
+axios.defaults.headers.get['Accepts'] = 'application/json';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept';
+
 
 function App() {
   return (
